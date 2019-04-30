aef644acea84df5cf7844e3c36f784015486e32d
Tovia Werzberger <tovia0567@gmail.com>
2019-04-30 13:18:12 -0400

    Add project files.

---
+++ EFHmwk_4_30_2019/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EFHmwk_4_30_2019.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
